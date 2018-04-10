bff7090c6fd449c4f4b9e9cc9288095b57cd19fc
Franklin Matheus <franklinmatheus1@gmail.com>
2018-04-10 17:36:04 -0300

    overview e login com css

--- src/main/java/com/imd/telemaco/entity/Serie.java
+++ src/main/java/com/imd/telemaco/entity/Serie.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.imd.telemaco.entity;
+import com.imd.telemaco.entity.enums.Classification;
 import java.util.ArrayList;
 
 /**
@@ -11,96 +12,80 @@ import java.util.ArrayList;
  * @author valmir
  */
 public class Serie {
+    private int id;
     private String name;
     private String year;
     private ArrayList<Season> seasons;
     public Rating rating;
+    private int id_creator;
+    private Classification classification;
     
     public Serie() { }
-    
-    public Serie( String name, String year, ArrayList<Season> seasons, Rating rating) { 
+
+    public Serie(int id, String name, String year, ArrayList<Season> seasons, Rating rating, int id_creator, Classification classification) {
+        this.id = id;
         this.name = name;
         this.year = year;
         this.seasons = seasons;
         this.rating = rating;
+        this.id_creator = id_creator;
+        this.classification = classification;
     }
     
-    /**
-     * @return the name
-     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the year
-     */
     public String getYear() {
         return year;
     }
 
-    /**
-     * @param year the year to set
-     */
     public void setYear(String year) {
         this.year = year;
     }
 
-     /**
-     * Verifica todo array de seasons para ver se esta compelto.
-     * @return the completed
-     */
-    public boolean isCompleted() {
-        
-        for (Season s : seasons) {
-            if (s.isCompleted() == false) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-
-    /**
-     * @return the seasons
-     */
     public ArrayList<Season> getSeasons() {
         return seasons;
     }
 
-    /**
-     * @param seasons the seasons to set
-     */
     public void setSeasons(ArrayList<Season> seasons) {
         this.seasons = seasons;
     }
-    
-    /**
-     * @return the stars of rating
-     */
-    public double stars() {
-        return this.rating.stars;
-    }
 
-    /**
-     * @return the rating
-     */
     public Rating getRating() {
         return rating;
     }
 
-    /**
-     * @param rating the rating to set
-     */
     public void setRating(Rating rating) {
         this.rating = rating;
+    }
+
+    public int getId_creator() {
+        return id_creator;
+    }
+
+    public void setId_creator(int id_creator) {
+        this.id_creator = id_creator;
+    }
+
+    public Classification getClassification() {
+        return classification;
+    }
+
+    public void setClassification(Classification classification) {
+        this.classification = classification;
     }
     
     @Override
--- src/main/webapp/Login.jsp
+++ src/main/webapp/Login.jsp
@@ -7,19 +7,52 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Login</title>
-    </head>
-    <body>
-        <p><a href="Index.jsp">Home </a>></p>
-        <h1>Login</h1>
-        <p>
-        <form name="register" action="LoginUser" method="POST">
-            <p><span>Email</span><input type="email" name="email" required /></p>
-            <p><span>Senha</span><input type="password" name="password" required /></p>
-            <p><input type="submit" value="Login" /></p>
-        </form>
-        </p>
-    </body>
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <style type="text/css"></style>
+    <link rel="stylesheet" type="text/css" href="./resources/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./resources/css/styleLogin.css">
+    <link rel="icon" type="png" href="./resources/media/images/icon.png">
+</head>
+<body>
+    <header>
+	<a href="./Overview.jsp">
+            <img src="./resources/media/images/icon.png" height="80%"> 
+            Telemaco
+        </a>
+    </header>
+
+    <div id="middle">
+    <form name="register" action="LoginUser" method="post">
+        <label id="title"><b>Login</b></label>
+
+	<div id="form">
+            <label for="email">Email</label>
+            <input type="email" name="email" class="inputText" placeholder="user@email.me" required>
+
+            <label for="psd">Senha</label>
+            <input type="password" name="password" class="inputText" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+        </div>
+
+	<div id="formObs">
+            <input type="checkbox" name="cbKeepConcected">
+            <label for="cbKeepConcected"> Mantenha-me Conectado </label>
+            <br>
+
+            <label> Não possui cadastro ainda? 
+            <a href="./Register.jsp">Cadastre-se agora!</a>
+            </label>
+        </div>
+        
+         <div id="buttons">
+             <a href="./Overview.jsp" type="button"><button class="button" id="btnCancel">Cancelar</button></a>
+            <button class="button" id="btnConfirm" type="submit" value="Login">Confirmar</button>
+	</div>
+    </form>
+
+       
+    
+    </div>
+</body>
 </html>
