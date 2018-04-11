755b6ed2239763cae78f4a365b7e329c6e3a3116
Shirley Ohara Telémaco <31323126+Telemaco98@users.noreply.github.com>
2018-04-11 17:37:42 -0300

    Update SeasonDAO.java

--- src/main/java/com/imd/telemaco/data/SeasonDAO.java
+++ src/main/java/com/imd/telemaco/data/SeasonDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.imd.telemaco.data;
 
 import java.sql.Connection;
@@ -10,24 +5,29 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.imd.telemaco.entity.Season;
+import com.imd.telemaco.entity.Episode;
 
 /**
  * 
  * @author  Shirley Ohara (shirleyohara@ufrn.edu.br)
  * @version 10.04.2018
  */
-public class SeasonDAO /*implements DAO*/{
-    private final Connection connection; 			/*< connection - it do the connection with the database. */
-    private static final Season seasonDAO = null;   /*< seasonDAO   */
+public class SeasonDAO implements DAO<Season> {
+    private Connection connection;
+    private Statement stm;
+    private static SeasonDAO seasonDAO = null;
     
-    private SeasonDAO () throws SQLException {
+    public SeasonDAO () throws SQLException {
     	this.connection = ConnectionFactory.getConnection();
     }
     
-    public static synchronized SeasonDAO getInstance () {
-    	throw new UnsupportedOperationException("Not supported yet.");
+    public static synchronized SeasonDAO getInstance () throws SQLException {
+    	if (seasonDAO == null) 
+    		seasonDAO = new SeasonDAO();
+    	return seasonDAO;
     }
     
     /**
@@ -36,12 +36,13 @@ public class SeasonDAO /*implements DAO*/{
      * @throws SQLException
      */
     public void insert (Season season) throws SQLException {
-    	String sql = "INSERT INTO season (id) VALUES (?)";
+    	String sql = "INSERT INTO telemaco.season (number, fkIdSerie, amountEp) VALUES (?, ?, ?)";
     	try {
     		PreparedStatement statement = connection.prepareStatement(sql);
-    		statement.setInt(1, season.getNumber());
-    		statement.setInt(2, season.getEpAmount());
-    		statement.execute();
+			statement.setInt(1, season.getNumber());
+			statement.setInt(2, season.getIdSerie());
+			statement.setInt(3, season.getEpAmount());
+			statement.execute();
     	} catch (SQLException e) {
     		throw new RuntimeException(e);
     	} finally {
@@ -51,26 +52,103 @@ public class SeasonDAO /*implements DAO*/{
     
     /**
      * Select a season from the database since the name
-     * @param  number
+     * @param  id
      * @return season
+     * @throws SQLException 
      */
-    public Season select (int number) {
-    	String sql = "SELECT * FROM season WHERE number = '" + number + "'";
+    public Season select (int id) throws SQLException {
+    	String sql = "SELECT * FROM telemaco.season WHERE id = '" + id + "'";
     	Season season = new Season();
+    	
     	try {
-    		Statement statement = connection.createStatement();
-    		ResultSet result = statement.executeQuery(sql);
-    		
-    		connection.close();
+    		stm = connection.createStatement();
+    		ResultSet result = stm.executeQuery(sql);
     		
     		boolean isThere = result.next();
     		
-    		if (isThere) season.setNumber(number);
-    		else season = null;
+    		if (isThere) {
+    			int number	  = result.getInt("number");
+    			EpisodeDAO epDAO = new EpisodeDAO();
+    			ArrayList <Episode> episodes = epDAO.selectAllEpisodes(id);
+    			int fkIdSerie = result.getInt("fkIdSerie");
+    			
+    			season = new Season (id, number, episodes, fkIdSerie);
+    		} else season = null;
     		
     		return season;
     	} catch (SQLException e) {
     		throw new RuntimeException(e);
+    	} finally {
+    		stm.close();
+    		connection.close();
     	}
     }
+    
+    /**
+     * Returns all the episodes of the Season
+     * @param  id
+     * @return episodes
+     * @throws SQLException
+     */
+    public ArrayList<Season> selectAllSeasons (int idSerie) throws SQLException {
+    	ArrayList <Season> seasons = new ArrayList <Season>();
+    	String sql = "SELECT * FROM telemaco.season WHERE fkIdSerie = '" + idSerie + "'";
+    	
+    	try {
+    		stm = connection.createStatement();
+    		ResultSet result = stm.executeQuery(sql);
+    		
+    		while (result.next()) {
+    			int idSeason = result.getInt("id");
+    			Season s = select(idSeason);
+    			seasons.add(s);
+    		}
+			
+    		return seasons;
+    	} catch (SQLException e) {
+    		throw new RuntimeException(e);
+    	} finally {
+    		stm.close();
+    		connection.close();
+    	}
+    }
+
+	@Override
+	public void delete(Season season) throws SQLException {
+		String sql = "REMOVE FROM telemaco.season WHERE id ='" + season.getId() + "'";
+		
+		try {
+			stm = connection.createStatement();
+			stm.execute(sql);
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		} finally {
+			stm.close();
+			connection.close();
+		}
+	}
+
+	@Override
+	public void update(Season season) throws SQLException {
+		String sql = "UPDATE FROM telemaco.season "
+				+ "number=?, "
+				+ "amountEp=?, "
+				+ "fkIdSeason=? "
+				+ "WHERE id=?"; 
+		
+		try {
+			PreparedStatement pStm = connection.prepareStatement(sql);//
+			pStm.setInt(1, season.getNumber());
+			pStm.setInt(2, season.getEpAmount());
+			pStm.setInt(3, season.getIdSerie());
+			pStm.setInt(4, season.getId());
+			pStm.execute();
+			
+			System.out.println("SUCESS");
+		} catch (SQLException e) {
+			System.out.println("EXCEPTION");
+		} finally {
+			connection.close();
+		}
+	}
 }
